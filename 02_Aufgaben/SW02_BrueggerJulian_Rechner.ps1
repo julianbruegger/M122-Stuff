9af862f5d903041059e4a9280c70c8afc87a16d0
Julian Bruegger <julian.bruegger@ict-bz.ch>
2020-08-27 10:03:25 +0200

    Powershell-SW02

---
+++ 02_Aufgaben/SW02_BrueggerJulian_Rechner.ps1
@@ -0,0 +1,23 @@
+﻿$numbers = @(1,2,3,4,5,6,7,8,9,10)
+
+$1 = 0
+
+while($true){
+    $Userinput = Read-Host -Prompt  "Zahl zwischen -10 und +10"
+
+    if ($Userinput -eq 0){
+            Write-Host Das endergebnis ist $1
+    }
+
+    else {
+
+        if ($Userinput -in $numbers) {
+            $1 = $1 + $Userinput 
+            }
+            else{
+            Write-Host "Value is negative"
+
+            }
+        }
+
+}
---
+++ 02_Aufgaben/SW02_Script_BrueggerJulian.ps1
@@ -0,0 +1,5 @@
+﻿$1 = 1
+    Write-Host $1
+$1 = $1 +4
+$1 = $1 -2
+    Write-Host Das Resultat ist $1
